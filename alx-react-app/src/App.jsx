47635694b982800e36248f95ef1d6c6f57eb2292
Mlaahtony <tonymn06@proton.me>
2025-02-23 23:26:03 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+import UserProfile from './components/UserProfile';
+
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -13,17 +15,19 @@ function App() {
 
   return (
     <>
-      function App() {
-  return (
+     <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
     <div>
       <Header />
       <MainContent />
       <Footer />
     </div>
-  );
-}
-
-export default App;
+  
 
        <div>
       <WelcomeMessage />
