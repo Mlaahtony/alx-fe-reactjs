1f33cf22c95d9871cbe2231942203897204ea5c3
Mlamuli Ncube <tonymn06@proton.me>
2025-03-28 01:08:57 +0200

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import React from 'react'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -9,6 +14,9 @@ function App() {
 
   return (
     <>
+    <Header/>
+    <MainContent/>
+    <Footer/>
     <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
