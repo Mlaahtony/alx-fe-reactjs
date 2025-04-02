49d23e81144ad407c3373f69f6f6eaf7c722cad2
Mlamuli Ncube <tonymn06@proton.me>
2025-04-02 23:23:20 +0200

    Added Header, MainContent and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,8 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -9,6 +14,11 @@ function App() {
 
   return (
     <>
+      <div>
+       <Header/>
+       <MainContent/>
+       <Footer/>
+      </div>
 
       <div>
       <WelcomeMessage />
