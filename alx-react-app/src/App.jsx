88ccd7aacb0bf8363ada8909f55d2b59a65ea8ce
Mlamuli Ncube <tonymn06@proton.me>
2025-04-03 00:17:04 +0200

    modified App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -6,7 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import UserProfile from './components/UserProfile';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,9 +16,9 @@ function App() {
     <>
       <div>
         <UserProfile
-         name="Mlamuli"
-         age= "32"
-         bio= "Loves Hiking"
+         name="Alice"
+         age= "25"
+         bio= "Loves hiking and photography"
          />
       </div>
       <div>
