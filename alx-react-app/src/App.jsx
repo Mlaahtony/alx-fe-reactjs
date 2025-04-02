ef1d2a0c745263427625c03dea4d13005939475b
Mlamuli Ncube <tonymn06@proton.me>
2025-04-02 23:51:13 +0200

    added UserProfile.jsx and imported it in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,19 +1,25 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
-
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <UserProfile
+         name="Mlamuli"
+         age= "32"
+         bio= "Loves Hiking"
+         />
+      </div>
       <div>
       <Header />
       <MainContent />
